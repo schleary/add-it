81354223f1b70f255f77d58dbddd359c06670864
Holly Leary <schleary@gmail.com>
2014-11-02 14:14:27 -0800

    init addit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,49 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(find_params)
+    if @item.save
+      redirect_to items_path(@item)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(find_params)
+      redirect_to items_show_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def find_params
+    params.require(:item).permit(:title, :author, :link, :description)
+  end
+
+end
